9264becf80c15c815565e0e1dd4785158a148cfe
Hussein Hassan <01096694296hud@gmail.com>
2026-06-08 04:49:37 +0400

    fix: lazy initialize Prisma in serverless

--- api/src/modules/prisma/prisma.service.ts
+++ api/src/modules/prisma/prisma.service.ts
@@ -1,12 +1,8 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-
+export class PrismaService extends PrismaClient {
   async enableShutdownHooks(app: INestApplication) {
     process.on('beforeExit', async () => {
       await app.close();
